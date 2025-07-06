1dfb9b62e9d056eb8e8cff28972321976bb2c733
whf <whf200608@163.com>
2025-07-06 17:31:31 +0800

    Update train.py

--- cfdnet/train.py
+++ cfdnet/train.py
@@ -12,28 +12,16 @@ from optimizers_schedulers import get_optimizer_scheduler, update_paramter
 
 def train(show=None, args=None):
     config_path = args['config']
-    work_space = args['work_dir']
-    flag = args['resume_from']
-    start_epoch = args['start_epoch']
-    end_epoch = args['end_epoch']
-    per_n = args['per_n']
-    pretrain = args['pretrain']
-    weights_path = args['weight_path']
-    
+
     ###1. 配置参数
     if not Path(config_path).exists():
         print('not exists :', config_path)
         return
+        
     cfg = json.load(open(config_path))
-    if args['img_size']:
-        cfg["training"]["img_size"] = (args['img_size'],args['img_size'])
-    if args['batch_size']:
-        cfg["training"]["batch_size"] = args['batch_size']
-    if end_epoch is None:
-        end_epoch = cfg["training"]["end_epoch"]
-    
     cfg["rdd"]=args["rdd"]
     n_classes = cfg['data']['n_classes']
+    
     save_path = init_file(work_space=work_space,model_name=cfg['model'],flag=flag)
     with open(save_path+'/model_config.json','w') as f:
         f.write(json.dumps(cfg))
@@ -42,22 +30,10 @@ def train(show=None, args=None):
     trainloader, valloader = get_dataloader(cfg)
     
     ###3. 加载权重
-    cfg['model_param'].update({"n_classes":n_classes})
     model = torch.nn.parallel.DataParallel(model_dic[cfg['model']](**cfg['model_param']))
     print(model)
 
     
-    if flag:
-        if Path(save_path+'/log.txt').exists():
-            with open(save_path+'/log.txt','r') as f:
-                line = f.readlines()[-1]
-                start_epoch=int(line.split(" ")[1])
-                
-        load_weights= save_path+'/model.pt'
-
-        if Path(load_weights).exists():
-            print('loading weights', load_weights)
-            model.load_state_dict(torch.load(load_weights, map_location='cpu'),strict=False)
     if pretrain:
         print('loading pretrain weights', weights_path)
         model.load_state_dict(torch.load(weights_path, map_location='cpu'))
@@ -84,38 +60,6 @@ def train(show=None, args=None):
 if __name__ == '__main__':
     args = parse_args()
     args = args.__dict__
-    # args['config'] = 'model_config/model_FDD_model_multitask_seg_dataset_imgs8.json'
-    # args['config'] = './model_config/model_ccnet_r50-d8_seg_dataset.json'
-    # args['config'] = 'model_config/model_A2FPN_seg_dataset.json'
-    # args['config'] = 'model_config/model_fpn_poolformer_s12_seg_dataset.json'
-    # args['config'] = 'model_config/model_danet_r50-d8_seg_dataset.json'
-    # args['config'] = 'model_config/model_dmnet_r50-d8_seg_dataset.json'
-    # args['config'] = 'model_config/model_vit_model_seg_dataset.json'
-    # args['config'] = 'model_config/model_base_unet_segmuilt_datasetv2.json'
-    # args['config'] = 'model_config/model_vgg_unet_seg_dataset_harvey.json'
-    # args['config'] = 'model_config/model_UNetFormerdpn98_seg_dataset.json'
-    # args['config'] = 'model_config/model_deeplabv3plus_r50-d8_seg_dataset.json'
-    # args['config'] = 'model_config/model_fcn_r50-d8_seg_dataset.json'
-    # args['config'] = 'model_config/model_fcn_hr18_seg_dataset.json'
-    # args['config'] = 'model_config/model_fast_scnn_seg_dataset.json'
-    # args['config'] = 'model_config/model_san_vit-b16_seg_dataset.json'
-    # args['config'] = 'model_config/model_emanet_r50-d8_seg_dataset.json'
-    # args['config'] = 'model_config/model_encnet_r50-d8_seg_dataset.json'
-    # args['config'] = 'model_config/model_erfnet_fcn_seg_dataset.json'
-    # args['config'] = 'model_config/model_vgg_unet_seg_dataset_imgs8.json'
-    #args['config'] = 'model_config/model_bisenetv2_seg_dataset_imgs8.json'
-    # args['config'] = 'model_config/model_UNetFormer_seg_dataset_imgs8.json'
-    #args['config'] = 'model_config/model_ccnet_r50-d8_seg_dataset_imgs8.json'
-    #args['config'] = 'model_config/model_deeplabv3_r50-d8_seg_dataset_imgs8.json'
-    args['config'] = 'model_config/model_erfnet_fcn_seg_dataset_imgs8.json'
-
-
-    args['img_size'] = None
-    args['batch_size'] =None
-    args["rdd"] = False
-    args['resume_from'] =None
-    args['per_n'] = 1
-    args['weight_path'] = ""
-    args['pretrain'] =False
-    # args['end_epoch'] = 30
+    args['config'] = 'model_config/model_cfdnet_seg_dataset.json'
+    args["rdd"] = True
     train(args=args, show=None)
