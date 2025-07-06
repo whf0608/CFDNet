23c2be13682a0464bab9fcfee15d614a818f4475
whf <whf200608@163.com>
2025-07-06 17:22:48 +0800

    Update train_main.py

--- cfdnet/train_main.py
+++ cfdnet/train_main.py
@@ -1,16 +1,16 @@
 import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 import sys
+sys.path.append("../models_seg")
 import json
 from init import *
 from train_model import train_model
-from tran_process import Train_Precess
 from models import get_model,model_dic
 from lossers import get_loss_function
 from vdatasets import get_dataloader
 from optimizers_schedulers import get_optimizer_scheduler, update_paramter
 
 def train(show=None, args=None):
-
     config_path = args['config']
     work_space = args['work_dir']
     flag = args['resume_from']
@@ -80,3 +80,42 @@ def train(show=None, args=None):
                 optimizer=optimizer, scheduler=scheduler, get_loss_func=get_train_loss_func,
                 get_val_func=get_val_loss_func, per_n=per_n, update_lr_paramter=update_lr_paramter)
 
+
+if __name__ == '__main__':
+    args = parse_args()
+    args = args.__dict__
+    # args['config'] = 'model_config/model_FDD_model_multitask_seg_dataset_imgs8.json'
+    # args['config'] = './model_config/model_ccnet_r50-d8_seg_dataset.json'
+    # args['config'] = 'model_config/model_A2FPN_seg_dataset.json'
+    # args['config'] = 'model_config/model_fpn_poolformer_s12_seg_dataset.json'
+    # args['config'] = 'model_config/model_danet_r50-d8_seg_dataset.json'
+    # args['config'] = 'model_config/model_dmnet_r50-d8_seg_dataset.json'
+    # args['config'] = 'model_config/model_vit_model_seg_dataset.json'
+    # args['config'] = 'model_config/model_base_unet_segmuilt_datasetv2.json'
+    # args['config'] = 'model_config/model_vgg_unet_seg_dataset_harvey.json'
+    # args['config'] = 'model_config/model_UNetFormerdpn98_seg_dataset.json'
+    # args['config'] = 'model_config/model_deeplabv3plus_r50-d8_seg_dataset.json'
+    # args['config'] = 'model_config/model_fcn_r50-d8_seg_dataset.json'
+    # args['config'] = 'model_config/model_fcn_hr18_seg_dataset.json'
+    # args['config'] = 'model_config/model_fast_scnn_seg_dataset.json'
+    # args['config'] = 'model_config/model_san_vit-b16_seg_dataset.json'
+    # args['config'] = 'model_config/model_emanet_r50-d8_seg_dataset.json'
+    # args['config'] = 'model_config/model_encnet_r50-d8_seg_dataset.json'
+    # args['config'] = 'model_config/model_erfnet_fcn_seg_dataset.json'
+    # args['config'] = 'model_config/model_vgg_unet_seg_dataset_imgs8.json'
+    #args['config'] = 'model_config/model_bisenetv2_seg_dataset_imgs8.json'
+    # args['config'] = 'model_config/model_UNetFormer_seg_dataset_imgs8.json'
+    #args['config'] = 'model_config/model_ccnet_r50-d8_seg_dataset_imgs8.json'
+    #args['config'] = 'model_config/model_deeplabv3_r50-d8_seg_dataset_imgs8.json'
+    args['config'] = 'model_config/model_erfnet_fcn_seg_dataset_imgs8.json'
+
+
+    args['img_size'] = None
+    args['batch_size'] =None
+    args["rdd"] = False
+    args['resume_from'] =None
+    args['per_n'] = 1
+    args['weight_path'] = ""
+    args['pretrain'] =False
+    # args['end_epoch'] = 30
+    train(args=args, show=None)
